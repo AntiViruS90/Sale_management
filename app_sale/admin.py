600a733bb588d9a0ab3e54be6daff60820b6900f
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2023-11-24 22:36:13 +0300

    addcart.py; admin.py; urls.py

--- app_sale/admin.py
+++ app_sale/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_sale.models.category import Category
+from app_sale.models.tag import Tag
+from app_sale.models.inventory import Inventory
+from app_sale.models.product import Product
+from app_sale.models.order import Order, OrderItem
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'parent', 'created_at', 'update_at']
+    list_display_links = ['name', 'parent']
+    list_filter = ['name']
+    list_per_page = 8
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag)
+
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category_name', 'current_stock', 'purchase_price',
+                    'sales_price', 'promotional_price']
+    list_display_links = ['name', 'category_name']
+    list_editable = ['current_stock', 'purchase_price', 'sales_price', 'promotional_price']
+    list_filter = ['name', 'current_stock']
+    search_fields = ['name', 'purchase_price', 'sales_price', 'promotional_price']
+    list_per_page = 8
+
+
+admin.site.register(Inventory, InventoryAdmin)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
