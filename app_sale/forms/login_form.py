7112e60c7b4761cddec4647f2ef269672d2db4c3
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2023-11-27 23:39:03 +0300

    frontend

--- app_sale/forms/login_form.py
+++ app_sale/forms/login_form.py
@@ -4,9 +4,9 @@ from django.contrib.auth.forms import AuthenticationForm
 
 class LoginForm(AuthenticationForm):
     username = f.CharField(widget=f.TextInput(
-        attrs={'class': 'form-control form-control-lg', 'placeholder': 'Username', 'required': True,
+        attrs={'class': 'form-control input_login', 'placeholder': 'Username', 'required': True,
                'autofocus': True}))
     password = f.CharField(
         widget=f.PasswordInput(
-            attrs={'class': 'form-control form-control-lg', 'placeholder': 'Password', 'required': True})),
+            attrs={'class': 'form-control input_login', 'placeholder': 'Password', 'required': True})),
     remember_me = f.BooleanField(required=False)
